cec5563dac3211fc5cccd81805a034ff6c6d781f
chaitanya5 <chaitanyarout5@gmail.com>
2019-10-16 01:26:29 +0530

    15oct

---
+++ test.js
@@ -0,0 +1,56 @@
+//Variables
+// let message = 'Hello there!'
+// var message2 = 50
+// const msg = `${message} 19 ${message2}`
+// alert(msg)
+
+//Data Types
+
+// let age = null
+// if(!age)
+//     alert(age)
+// console.log(typeof 0)
+// console.log(true)
+// console.log('Hello')
+// console.log(Math)
+// console.log(console.log)
+// console.log(alert)
+// console.log(Symbol)
+
+// Type Conversion
+// status = true
+// alert(status)
+// status = String(status)
+// alert(typeof(status))
+
+let msg = 'Hello there'
+const num = null
+// alert(Number(msg))
+// alert(Number(num))
+// alert(null(msg)) //Shows error
+// alert(Boolean(msg))
+// alert(Boolean(0))
+// alert(Boolean(''))
+
+//Operators
+
+// alert(2+ 2+ '1'+'1'+2 + 2) //41122
+// alert(- + true) //-1
+// alert(+ + true) //1
+// alert(- - false)//False always gives 0
+// alert(+'') //Unary conversion
+
+// Precedence
+// Unary Add/Sub > Mult/Div > Add/Sub > Assignment > Comma
+
+//Multiple Assignments
+// c = 4- (a=5+8)
+// alert(c) // -9
+
+//alert(4 ** 2)// 16
+//alert(parseInt('1001',2))//Gives binary representation
+//alert(0111 && 0101)//0101 // 65 
+//alert('0101' & '1101')//65
+// alert(a = 1 + 2, 3 + 4) // Comma has least precedence so 1+2 is assigned first
+
+
